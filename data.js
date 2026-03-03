6608b55229ba5549cc484c8f6c9744dd9251b4da
mwcrox <97325935+mwcrox@users.noreply.github.com>
2026-03-03 14:14:34 -0700

    Add ESPN URLs for tournament schedules
    Added ESPN URLs for various college basketball tournaments.

--- data.js
+++ data.js
@@ -4,185 +4,216 @@ window.CONFERENCE_TOURNAMENTS = [
         champISO: "2026-03-14T11:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/1"
     },
     {
         key: "American", start: "2026-03-11", end: "2026-03-15",
         champISO: "2026-03-15T15:15:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/62"
     },
     {
         key: "Atlantic 10", start: "2026-03-11", end: "2026-03-15",
         champISO: "2026-03-15T13:00:00-04:00",
         channel: "CBS",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/3"
     },
     {
         key: "ACC", start: "2026-03-10", end: "2026-03-14",
         champISO: "2026-03-14T20:30:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/2"
     },
     {
         key: "ASUN", start: "2026-03-04", end: "2026-03-08",
         champISO: "2026-03-08T14:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/46"
     },
     {
         key: "Big 12", start: "2026-03-10", end: "2026-03-14",
         champISO: "2026-03-14T18:00:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/8"
     },
     {
         key: "Big East", start: "2026-03-11", end: "2026-03-14",
         champISO: "2026-03-14T18:30:00-04:00",
         channel: "FOX",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/4"
     },
     {
         key: "Big Sky", start: "2026-03-07", end: "2026-03-11",
         champISO: "2026-03-11T23:30:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/5"
     },
     {
         key: "Big South", start: "2026-03-04", end: "2026-03-08",
         champISO: "2026-03-08T12:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/6"
     },
     {
         key: "Big Ten", start: "2026-03-10", end: "2026-03-15",
         champISO: "2026-03-15T15:30:00-04:00",
         channel: "CBS",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/7"
     },
     {
         key: "Big West", start: "2026-03-11", end: "2026-03-14",
         champISO: "2026-03-14T19:00:00-07:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/8"
     },
     {
         key: "CAA", start: "2026-03-06", end: "2026-03-10",
         champISO: "2026-03-10T19:00:00-04:00",
         channel: "CBSSN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/10"
     },
     {
         key: "Conference USA", start: "2026-03-10", end: "2026-03-14",
         champISO: "2026-03-14T20:30:00-04:00",
         channel: "CBSSN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/11"
     },
     {
         key: "Horizon League", start: "2026-03-02", end: "2026-03-10",
         champISO: "2026-03-10T19:00:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/10"
     },
     {
         key: "Ivy League", start: "2026-03-14", end: "2026-03-15",
         champISO: "2026-03-15T12:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/12"
     },
     {
         key: "MAAC", start: "2026-03-05", end: "2026-03-10",
         champISO: "2026-03-10T21:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/13"
     },
     {
         key: "MAC", start: "2026-03-12", end: "2026-03-14",
         champISO: "2026-03-14T20:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/14"
     },
     {
         key: "MEAC", start: "2026-03-11", end: "2026-03-14",
         champISO: "2026-03-14T13:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/16"
     },
     {
         key: "Missouri Valley", start: "2026-03-05", end: "2026-03-08",
         champISO: "2026-03-08T12:00:00-04:00",
         channel: "CBS",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/18"
     },
     {
         key: "Mountain West", start: "2026-03-11", end: "2026-03-14",
         champISO: "2026-03-14T18:00:00-04:00",
         channel: "CBS",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/44"
     },
     {
         key: "Northeast", start: "2026-03-04", end: "2026-03-10",
         champISO: "2026-03-10T19:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/19"
     },
     {
         key: "Ohio Valley", start: "2026-03-04", end: "2026-03-07",
         champISO: "2026-03-07T20:00:00-05:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/20"
     },
     {
         key: "Patriot League", start: "2026-03-03", end: "2026-03-11",
         champISO: "2026-03-11T19:00:00-04:00",
         channel: "CBS Sports Network",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/22"
     },
     {
         key: "SEC", start: "2026-03-11", end: "2026-03-15",
         champISO: "2026-03-15T13:00:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/23"
     },
     {
         key: "SoCon", start: "2026-03-06", end: "2026-03-09",
         champISO: "2026-03-09T19:00:00-04:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/24"
     },
     {
         key: "Southland", start: "2026-03-08", end: "2026-03-11",
         champISO: "2026-03-11T17:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/25"
     },
     {
         key: "SWAC", start: "2026-03-09", end: "2026-03-14",
         champISO: "2026-03-14T19:30:00-04:00",
         channel: "ESPNU",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/26"
     },
     {
         key: "Summit League", start: "2026-03-04", end: "2026-03-08",
         champISO: "2026-03-08T21:00:00-04:00",
         channel: "CBSSN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/49"
     },
     {
         key: "Sun Belt", start: "2026-03-03", end: "2026-03-09",
         champISO: "2026-03-09T19:00:00-04:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/27"
     },
     {
         key: "West Coast", start: "2026-03-05", end: "2026-03-10",
         champISO: "2026-03-10T18:00:00-07:00",
         channel: "ESPN",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/29"
     },
     {
         key: "WAC", start: "2026-03-11", end: "2026-03-14",
         champISO: "2026-03-14T21:00:00-07:00",
         channel: "ESPN2",
         winner: null
+        espnUrl: "https://www.espn.com/mens-college-basketball/scoreboard/_/seasontype/2/group/30"
     }
 ];
